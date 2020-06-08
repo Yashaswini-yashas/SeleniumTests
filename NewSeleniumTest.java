5683f2aac8386a94d637a1a62d5ac2512c596a7e
root <root@ip-172-31-3-93.us-west-2.compute.internal>
2020-06-08 09:31:02 +0000

    first commit

---
+++ NewSeleniumTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class NewSeleniumTest {
+public static void main(String args[]) {
+int code = 0;
+String baseURL = "http://34.221.243.81:9000/";
+System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+ChromeOptions chromeOptions = new ChromeOptions();
+chromeOptions.addArguments("--headless");
+chromeOptions.addArguments("--no-sandbox");
+WebDriver driver = new ChromeDriver(chromeOptions);
+driver.get(baseURL + "/petclinic-Dev/owners/find.html");
+
+driver.findElement(By.xpath("//a[@class='btn btn-default']")).click();
+	driver.findElement(By.xpath("//input[@id='firstName']")).clear();
+	driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Thomas");
+	driver.findElement(By.xpath("//input[@id='lastName']")).clear();
+	driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Jefferson");
+	driver.findElement(By.xpath("//input[@id='address']")).clear();
+	driver.findElement(By.xpath("//input[@id='address']")).sendKeys("456 Clark Street");
+	driver.findElement(By.xpath("//input[@id='city']")).clear();
+	driver.findElement(By.xpath("//input[@id='city']")).sendKeys("Chicago");
+	driver.findElement(By.xpath("//input[@id='telephone']")).clear();
+	driver.findElement(By.xpath("//input[@id='telephone']")).sendKeys("3452987654");
+	driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+	System.out.println("user addition successful !");
+
+driver.navigate().to(baseURL + "/petclinic-Dev/owners/find.html");
+driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+WebElement Owner = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]"));
+String outputValue = Owner.getText();
+if(outputValue.contains("Thomas Jefferson")) {
+	System.out.println("Owner Found");
+}else {
+	System.out.println("Owner not found");
+           code = 2;
+}
+	
+	driver.quit();
+System.exit(code);
+}
+
+}
+
